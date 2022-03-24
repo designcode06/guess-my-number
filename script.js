329d5a339a141575b1d34dda3921d59b922bbcd7
designcode14 <micah.personal@icloud.com>
2022-03-24 11:11:52 -0700

    Add 'Enter' keydown event

--- script.js
+++ script.js
@@ -8,18 +8,15 @@ const displayMessage = function (message) {
   document.querySelector(".message").textContent = message;
 };
 
-document.querySelector(".check").addEventListener("click", function () {
+const checkGuess = function () {
   const guess = Number(document.querySelector(".guess").value);
-  console.log(guess, typeof guess);
 
   // WHEN THERE IS NO INPUT
   if (!guess) {
-    // document.querySelector(".message").textContent = "⛔️ No number!";
     displayMessage("⛔️ No number!");
 
     // WHEN PLAYER WINS
   } else if (guess === secretNumber) {
-    // document.querySelector(".message").textContent = "🎉 Correct Number!";
     displayMessage("🎉 Correct Number!");
     document.querySelector(".number").textContent = secretNumber;
 
@@ -34,44 +31,24 @@ document.querySelector(".check").addEventListener("click", function () {
     // WHEN GUESS IS WRONG
   } else if (guess !== secretNumber) {
     if (score > 1) {
-      // document.querySelector(".message").textContent =
-      //   guess > secretNumber ? "⬆️ Too high!" : "⬇️ Too low!";
       displayMessage(guess > secretNumber ? "⬆️ Too high!" : "⬇️ Too low!");
       score--;
       document.querySelector(".score").textContent = score;
     } else {
-      // document.querySelector(".message").textContent = "😭 You lost the game!";
       displayMessage("😭 You lost the game!");
       document.querySelector(".score").textContent = 0;
     }
   }
+};
 
-  //   // WHEN GUESS IS TOO HIGH
-  // } else if (guess > secretNumber) {
-  //   if (score > 1) {
-  //     document.querySelector(".message").textContent = "⬆️ Too high!";
-  //     score--;
-  //     document.querySelector(".score").textContent = score;
-  //   } else {
-  //     document.querySelector(".message").textContent = "😭 You lost the game!";
-  //     document.querySelector(".score").textContent = 0;
-  //   }
+document.querySelector(".check").addEventListener("click", checkGuess());
 
-  //   // WHEN GUESS IS TOO LOW
-  // } else if (guess < secretNumber) {
-  //   if (score > 1) {
-  //     document.querySelector(".message").textContent = "⬇️ Too low!";
-  //     score--;
-  //     document.querySelector(".score").textContent = score;
-  //   } else {
-  //     document.querySelector(".message").textContent = "😭 You lost the game!";
-  //     document.querySelector(".score").textContent = 0;
-  //   }
-  // }
+document.querySelector(".guess").addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    checkGuess();
+  }
 });
 
-///////////////////////////////////
-// Coding Challenge #1
 document.querySelector(".again").addEventListener("click", function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
